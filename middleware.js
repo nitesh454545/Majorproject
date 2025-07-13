cd09a8dcbc86cb653ce0d018257f6397d21e4762
Nitesh Prajapati <nitesh454545@gmail.com>
2025-07-13 19:25:07 +0530

    Add Project Files

---
+++ middleware.js
@@ -0,0 +1,68 @@
+const Listing = require("./models/listing");
+const Review = require("./models/review");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const ExpressError = require("./utils/ExpressError.js");
+
+const isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    res.session.redirectUrl = req.originalUrl;
+    req.flash("error", "You must be logged in to create a listing!");
+    return res.redirect("/login");
+  }
+  next();
+};
+
+const saveRedirectUrl = (req, res, next) => {
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
+  }
+  next();
+};
+
+const isReviewAuthor = async (req, res, next) => {
+  let { id, reviewId } = req.params;
+  let review = await Review.findById(reviewId);
+  if (!review.author.equals(res.locals.currUser._id)) {
+    req.flash("error", "You are not the author of this listing");
+    return res.redirect(`/listings/${id}`);
+  }
+  next();
+};
+
+const validateListing = (req, res, next) => {
+  const { error } = listingSchema.validate(req.body);
+  if (error) {
+    const errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  }
+  next();
+};
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  }
+  next();
+};
+
+const isOwner = async (req, res, next) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  if (!listing.owner.equals(res.locals.currUser._id)) {
+    req.flash("error", "You are not authorized to do that!");
+    return res.redirect(`/listings/${id}`);
+  }
+  next();
+};
+
+// ✅ Export all as an object
+module.exports = {
+  isLoggedIn,
+  saveRedirectUrl,
+  isReviewAuthor,
+  validateListing,
+  validateReview,
+  isOwner
+};
